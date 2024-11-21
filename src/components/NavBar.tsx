6b21ae0ac6236d4ee0393e35ab26d2555753f696
Martin2000SVK <tacsikmartin@gmail.com>
2024-11-21 11:49:54 +0100

    done nav bar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -8,6 +8,7 @@ import { BottomNavigation, BottomNavigationAction, Box } from '@mui/material';
 import HomeIcon from '@mui/icons-material/Home';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import AddCircleIcon from '@mui/icons-material/AddCircle';
+import InfoIcon from '@mui/icons-material/Info';
 import { useRouter } from 'next/navigation';
 
 export default function Navbar() {
@@ -29,6 +30,7 @@ export default function Navbar() {
         <BottomNavigationAction label="Domov" value="/" icon={<HomeIcon />} />
         <BottomNavigationAction label="Profily" value="/profil" icon={<AccountCircleIcon />} />
         <BottomNavigationAction label="Príspevky" value="/prispevok" icon={<AddCircleIcon />} />
+        <BottomNavigationAction label="O mne" value="/o-mne" icon={<InfoIcon />} />
       </BottomNavigation>
     </Box>
   );
